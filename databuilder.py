b90d02e5d17f4f4a73041c7b2f21bb16189942de
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-06-16 09:58:33 +0200

    Update databuilder.py

--- databuilder.py
+++ databuilder.py
@@ -1,7 +1,6 @@
-from random import random, randrange, getrandbits, randint 
+from random import random, randrange, getrandbits, randint, sample 
 import pandas as pd
 import numpy as np
-import string
 
 # type_index = item type
 # item_index = item object
@@ -22,17 +21,30 @@ def create_items():
          items.append([x, create_item_names(x)])
     return items          
 
-def buy_item(type_index, item_index):
-    return created_items[type_index][item_index]
 
-def buy_items(type_indexes, item_indexes, created_items, size_of_dataset):
+# Defines the items buyed by a single customer
+def buy_customer_items(type_index, created_items):
     items = []
-    for x in range(size_of_dataset):
-        type_index = type_indexes[x]
+    item_indexes = sample(range(10), 6)
+    for x in range(6):
         item_index = item_indexes[x]
         item = created_items[type_index][1][item_index]
         items.append(item)
     return items
+
+# Defines the items buyed by all customers
+def buy_items(type_indexes, created_items, size_of_dataset):
+    items = []
+    for x in range(size_of_dataset):
+        items.append(buy_customer_items(type_indexes[x], created_items))
+    return items
+
+# Map the type_index to its name
+def buy_types(type_indexes, created_items, size_of_dataset):
+    items = []
+    for x in range(size_of_dataset):
+        items.append(created_items[type_indexes[x]][0])
+    return items    
 
 
 if __name__ == '__main__':
@@ -42,11 +54,12 @@ if __name__ == '__main__':
     size_of_dataset=1000
    
     type_indexes = np.random.randint(0, 3, size_of_dataset)
-    item_indexes = np.random.randint(0, 9, size_of_dataset)
     
-    buyed_items = buy_items(type_indexes, item_indexes, created_items, size_of_dataset)
+    buyed_items = buy_items(type_indexes, created_items, size_of_dataset)
+    buyed_types = buy_types(type_indexes, created_items, size_of_dataset)
     
-    raw_data = {'type': type_indexes, 'buyed_items': buyed_items}
+    
+    raw_data = {'type': buyed_types, 'buyed_items': buyed_items}
     
     df = pd.DataFrame(raw_data)
     
