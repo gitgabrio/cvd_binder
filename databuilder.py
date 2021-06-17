2e55f94c7c1889cafc077e9b0123529110b82ae0
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-06-17 11:26:22 +0200

    Update files. Add simple_input_data

--- databuilder.py
+++ databuilder.py
@@ -26,20 +26,20 @@ def create_items():
     return items          
 
 
-# Defines the items buyed by a single customer
-def buy_customer_items(type_index, created_items):
-    items = []
-    item_indexes = sample(range(number_of_items_per_type), number_of_items_per_customer)
+# Defines the items buyed by a single customer as a 0/1 vector (1 = buyed)
+def buy_customer_items():
+    items = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    buyed_items = sample(range(number_of_items_per_type), number_of_items_per_customer)
     for x in range(number_of_items_per_customer):
-        item_index = item_indexes[x]
-        items.append(item_index)
+        buyed_item = buyed_items[x] 
+        items[buyed_item] = 1 
     return items
 
 # Defines the items buyed by all customers
 def buy_all_items(type_indexes, created_items, size_of_dataset):
     items = []
     for x in range(size_of_dataset):
-        items.append(buy_customer_items(type_indexes[x], created_items))
+        items.append(buy_customer_items())
     return items
 
 # Map the type_index to its name
@@ -56,34 +56,13 @@ def buy_items(type_indexes, item_indexes, created_items, size_of_dataset):
         type_index = type_indexes[x]
         sub_items = []
         sub_items_indexes = item_indexes[x]
-        for y in range(number_of_items_per_customer):
-            item_index = sub_items_indexes[y]
-            sub_items.append(created_items[type_index][1][item_index])
+        for y in range(number_of_items_per_type):
+            if sub_items_indexes[y] == 1:
+                sub_items.append(created_items[type_index][1][y])
         items.append(sub_items)
     return items
 
-def buy_items_ids(item_indexes, size_of_dataset):
-    items = []
-    for x in range(size_of_dataset):
-        buyed_items_indexes = item_indexes[x]
-        sub_group_id = 0
-        for y in range(number_of_items_per_customer):
-            sub_group_id += buyed_items_indexes[y]
-        items.append(sub_group_id)
-    return items 
-
-# Define the group_id based on type and item buyed
-def buy_group(type_index, buy_items_id):
-    group_id = type_index * 10
-    if buy_items_id <= 20:
-        return group_id + 1
-    elif buy_items_id >= 30:
-        return group_id + 3
-    else:
-        return group_id + 2 
-    
-
-
+   
 if __name__ == '__main__':
     created_items = create_items()
     print(created_items)
@@ -92,15 +71,13 @@ if __name__ == '__main__':
    
     buyed_type_indexes = np.random.randint(0, number_of_types, size_of_dataset)
     buyed_items_indexes = buy_all_items(buyed_type_indexes, created_items, size_of_dataset)
-    buyed_items_ids = buy_items_ids(buyed_items_indexes, size_of_dataset)
-    buyer_group = list(map(lambda x, y: buy_group(x, y), buyed_type_indexes, buyed_items_ids))
     
     buyed_types = buy_types(buyed_type_indexes, created_items, size_of_dataset)
     buyed_items = buy_items(buyed_type_indexes, buyed_items_indexes, created_items, size_of_dataset)
     
     
     
-    raw_data = {'type': buyed_types, 'buyed_items': buyed_items,  'type_index': buyed_type_indexes, 'items_id': buyed_items_ids, 'buyer_group': buyer_group}
+    raw_data = {'type': buyed_types, 'buyed_items': buyed_items,  'type_index': buyed_type_indexes, 'item_indexes': buyed_items_indexes}
     
     df = pd.DataFrame(raw_data)
     
