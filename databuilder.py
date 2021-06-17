3008d5e105a39a1f3c50c8ec21d6d3f2aff84908
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-06-17 17:13:04 +0200

    Updateing files

--- databuilder.py
+++ databuilder.py
@@ -35,6 +35,22 @@ def buy_customer_items():
         items[buyed_item] = 1 
     return items
 
+def buy_customer_items_id(type_indexes, item_indexes, size_of_dataset):
+    items = []
+    for x in range(size_of_dataset):
+        type_index = type_indexes[x]
+        sub_items_indexes = item_indexes[x]
+        str_id = ''
+        for y in range(number_of_types):
+            for k in range(number_of_items_per_type):
+                if y == type_index:
+                    str_id += str(sub_items_indexes[k])
+                else:
+                    str_id += '0'
+        items.append(int(str_id))
+    return items
+    
+
 # Defines the items buyed by all customers
 def buy_all_items(type_indexes, created_items, size_of_dataset):
     items = []
@@ -63,6 +79,8 @@ def buy_items(type_indexes, item_indexes, created_items, size_of_dataset):
     return items
 
    
+
+
 if __name__ == '__main__':
     created_items = create_items()
     print(created_items)
@@ -71,15 +89,17 @@ if __name__ == '__main__':
    
     buyed_type_indexes = np.random.randint(0, number_of_types, size_of_dataset)
     buyed_items_indexes = buy_all_items(buyed_type_indexes, created_items, size_of_dataset)
+    buyed_customer_items_id = buy_customer_items_id(buyed_type_indexes, buyed_items_indexes, size_of_dataset)
     
     buyed_types = buy_types(buyed_type_indexes, created_items, size_of_dataset)
     buyed_items = buy_items(buyed_type_indexes, buyed_items_indexes, created_items, size_of_dataset)
     
     
     
-    raw_data = {'type': buyed_types, 'buyed_items': buyed_items,  'type_index': buyed_type_indexes, 'item_indexes': buyed_items_indexes}
+    raw_data = {'type': buyed_types, 'buyed_items': buyed_items,  'type_index': buyed_type_indexes, 'buyed_customer_items_id': buyed_customer_items_id}
     
     df = pd.DataFrame(raw_data)
-    
     print(df.head())
-    df.to_csv('input_data.csv', index=False)
+    
+    df.sort_values(by=['type'], ascending=True).to_csv('input_data.csv', index=False)
+   # df.to_csv('input_data.csv', index=False)
