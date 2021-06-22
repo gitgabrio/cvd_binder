7ebc3284f314ed23de588418a3a7622a814568b1
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-06-22 15:20:26 +0200

    Formatted databuilder.py

--- databuilder.py
+++ databuilder.py
@@ -9,42 +9,32 @@ item_types = ["Book", "Car", "PC"]
 number_of_items_per_customer = 6
 number_of_items_per_type = 10
 number_of_types = 3
+size_of_dataset = 1000
 
 
 def create_item(item_type, item_index):
-    return item_type + "-" + str(item_index)
+    return f"{item_type}-{str(item_index)}"
 
 
 def create_item_names(item_type):
-    items = []
-    for x in range(number_of_items_per_type):
-        items.append(create_item(item_type, x))
-    return items
+    return [create_item(item_type, x) for x in range(number_of_items_per_type)]
 
 
 def create_items():
-    items = []
-    for x in item_types:
-        for y in range(number_of_items_per_type):
-            items.append(create_item(x, y))
-    return items
+    return [create_item(x, y)
+            for x in item_types for y in range(number_of_items_per_type)]
 
 # Defines the items buyed by a single customer as a 0/1 30-column array (1
 # = buyed)
 
 
 def buy_customer_items(type_index):
-    items = []
     items_indexes = sample(
         range(number_of_items_per_type),
         number_of_items_per_customer)
     items_indexes = [element + (type_index * 10) for element in items_indexes]
-    for x in range(number_of_types * number_of_items_per_type):
-        if x in items_indexes:
-            items.append(1)
-        else:
-            items.append(casual_buy())
-    return items
+    return [1 if x in items_indexes else casual_buy()
+            for x in range(number_of_types * number_of_items_per_type)]
 
 # randomly returns 0 or 1
 
@@ -58,33 +48,28 @@ def casual_buy():
 
 
 # Defines the items buyed by all customers
-def buy_all_items(type_indexes, size_of_dataset):
-    items = []
-    for x in range(size_of_dataset):
-        items.append(buy_customer_items(type_indexes[x]))
-    return items
+def buy_all_items(type_indexes):
+    return [buy_customer_items(type_indexes[x])
+            for x in range(size_of_dataset)]
 
 # Map the type_index to its name
 
 
-def buy_types(type_indexes, item_types, size_of_dataset):
-    items = []
-    for x in range(size_of_dataset):
-        items.append(item_types[type_indexes[x]])
-    return items
+def buy_types(type_indexes):
+    return [item_types[type_indexes[x]] for x in range(size_of_dataset)]
 
 # Map the type_index/item_index to its name
 
 
-def buy_items(type_indexes, item_indexes, created_items, size_of_dataset):
+def buy_items(type_indexes, item_indexes, created_items):
     items = []
     for x in range(size_of_dataset):
         type_index = type_indexes[x]
-        sub_items = []
         sub_items_indexes = item_indexes[x]
-        for y in range(number_of_types * number_of_items_per_type):
-            if sub_items_indexes[y] == 1:
-                sub_items.append(created_items[y])
+        sub_items = [
+            created_items[y] for y in range(
+                number_of_types *
+                number_of_items_per_type) if sub_items_indexes[y] == 1]
         items.append(sub_items)
     return items
 
@@ -92,17 +77,14 @@ def buy_items(type_indexes, item_indexes, created_items, size_of_dataset):
 if __name__ == '__main__':
     created_items = create_items()
 
-    size_of_dataset = 1000
-
     buyed_type_indexes = np.random.randint(0, number_of_types, size_of_dataset)
-    buyed_items_indexes = buy_all_items(buyed_type_indexes, size_of_dataset)
+    buyed_items_indexes = buy_all_items(buyed_type_indexes)
 
-    buyed_types = buy_types(buyed_type_indexes, item_types, size_of_dataset)
+    buyed_types = buy_types(buyed_type_indexes)
     buyed_items = buy_items(
         buyed_type_indexes,
         buyed_items_indexes,
-        created_items,
-        size_of_dataset)
+        created_items)
 
     raw_data = {
         'type': buyed_types,
